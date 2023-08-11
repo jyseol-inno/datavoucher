16ab0ff25ed1805e63de8d197c26750cc83d4c65
HYUNJUNPARK <ekto7957@gmail.com>
2023-08-11 11:53:57 +0900

    ID찾기수정

---
+++ app_terms_board.py
@@ -0,0 +1,520 @@
+from flask import Flask, Response, request, jsonify, session, render_template, send_from_directory, send_file
+from flask_session import Session  # 서버 측 세션을 위한 Flask-Session 추가
+import mysql.connector
+import random
+from datetime import datetime
+import os
+import urllib.parse
+import string
+import datetime
+
+
+
+app = Flask(__name__)
+app.config['SESSION_TYPE'] = 'filesystem'  # 세션 데이터를 파일 시스템에 저장
+Session(app)  # 앱에 세션 설정 적용
+
+
+
+
+# MariaDB 연결 정보
+db_config = {
+    'host': '172.30.1.234',
+    'user': 'test',     # 사용자 이름에 맞게 변경해주세요
+    'password': 'test', # 비밀번호에 맞게 변경해주세요
+    'database': 'testdb3'   # 데이터베이스 이름에 맞게 변경해주세요
+}
+
+
+create_table_queries = [
+    '''
+    CREATE TABLE IF NOT EXISTS agreement (
+    MemberNo INT,
+    AgreeService BOOLEAN NOT NULL,
+    AgreePrivacy BOOLEAN NOT NULL,
+    AgreeMarketing BOOLEAN,
+    FOREIGN KEY(MemberNo) REFERENCES member_user(MemberNo)
+    );
+    ''',
+
+    '''
+    CREATE TABLE IF NOT EXISTS phone_verification (
+    PhoneNumber VARCHAR(20) PRIMARY KEY,
+    VerificationCode VARCHAR(6) NOT NULL,
+    Expiry DATETIME NOT NULL
+    );
+   
+    '''
+    
+]
+
+
+# MariaDB 연결 및 테이블 생성 쿼리문 여러개 실행
+conn = mysql.connector.connect(**db_config)
+cursor = conn.cursor()
+for query in create_table_queries:
+    cursor.execute(query)
+conn.commit()
+cursor.close()
+conn.close()
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+
+# -----------------------------------약관동의---------------------------------------
+@app.route('/agreement', methods=['POST'])
+def agreement():
+    data = request.get_json()
+
+    # 필수 필드 확인
+    required_fields = ['AgreeService', 'AgreePrivacy']
+    if not all(field in data for field in required_fields):
+        return jsonify({'error': '모든 필드를 입력해주세요'}), 400
+
+    agree_service = data['AgreeService']
+    agree_privacy = data['AgreePrivacy']
+    agree_marketing = data.get('AgreeMarketing', False)  # 마케팅 동의는 선택사항이므로 기본값을 False로 설정
+
+    # 세션에 동의 정보 저장 (나중에 회원가입 정보와 함께 데이터베이스에 저장)
+    session['agree_service'] = agree_service
+    session['agree_privacy'] = agree_privacy
+    session['agree_marketing'] = agree_marketing
+
+    return jsonify({'result': '약관 동의가 완료되었습니다'}), 200
+
+
+
+
+
+#---------------------------------가입시 이메일 인증---------------------------------------
+@app.route('/email_verification', methods=['POST'])
+def email_verification():
+    data = request.get_json()
+
+    # 필수 필드 확인
+    required_fields = ['Email_ID']
+    if not all(field in data for field in required_fields):
+        return jsonify({'error': '모든 필드를 입력해주세요'}), 400
+
+    email = data['Email_ID']
+
+    # 임의의 인증 코드 생성 (실제로는 이메일로 발송)
+    verification_code = '123456'
+
+    # 세션에 이메일과 인증 코드 저장
+    session['email'] = email
+    session['verification_code'] = verification_code
+
+    return jsonify({'result': '인증 코드가 발송되었습니다'}), 200
+
+
+@app.route('/verify_code', methods=['POST'])
+def verify_code():
+    data = request.get_json()
+
+    # 필수 필드 확인
+    required_fields = ['VerificationCode']
+    if not all(field in data for field in required_fields):
+        return jsonify({'error': '모든 필드를 입력해주세요'}), 400
+
+    user_code = data['VerificationCode']
+    server_code = session.get('verification_code')
+
+    if user_code != server_code:
+        return jsonify({'error': '인증 코드가 일치하지 않습니다'}), 400
+
+    # 인증 성공: 세션에 인증 상태 저장
+    session['verified'] = True
+
+    return jsonify({'message': '인증에 성공하였습니다'}), 200
+
+
+# -----------------------ID 찾기-----------------------------------------------------------------------------------------------------------------------------------------------
+def send_sms(phone_number, code):
+    # 문자메세지를 받았다 가정하는 함수. 실제로는 sms 수신 API를 구성해야 합나더,
+    print(f"Sending verification code {code} to phone number {phone_number}")
+
+# 인증번호 생성 및 저장
+def generate_and_save_verification_code(phone_number):
+    conn = mysql.connector.connect(**db_config)
+    cursor = conn.cursor(buffered=True)
+    # 번호 가입자 테이블에 휴대폰 번호 입력했는지 확인
+    cursor.execute("SELECT 1 FROM member_user WHERE PhoneNumber=%s", (phone_number,))
+    if not cursor.fetchone():
+        print("전화번호가 DB에 없습니다.") # 로깅 또는 다른 처리
+        cursor.close()
+        conn.close()
+        return None # DB에 저장하지 않고 None 반환
+
+    # Generate a random 6-digit code
+    code = ''.join(random.choices(string.digits, k=6))
+    # Save the code to the database with an expiry time of 10 minutes
+    expiry_time = datetime.datetime.now() + datetime.timedelta(minutes=10)
+
+    cursor.execute(
+        "INSERT INTO phone_verification (PhoneNumber, VerificationCode, Expiry) VALUES (%s, %s, %s) ON DUPLICATE KEY UPDATE VerificationCode=%s, Expiry=%s",
+        (phone_number, code, expiry_time, code, expiry_time)
+    )
+
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+    send_sms(phone_number, code)  # send_sms가 실제 문자를 보낸다고 가정
+
+    return code
+
+# 인증번호 요청
+@app.route('/ID_find/request_code', methods=['POST'])
+def request_verification_code():
+    try:
+        phone_number = request.json.get('phone_number')
+        session['phone_number'] = phone_number
+        if not phone_number:
+            return jsonify({"error": "휴대폰 번호 입력은 필수입니다."}), 400
+
+        code = generate_and_save_verification_code(phone_number)
+        if code is None:
+            return jsonify({"error": "가입하신 전화번호와 일치하지 않습니다."}), 400
+
+        return jsonify({"message": "인증코드를 보냈습니다.!", "verification_code": code}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+
+@app.route('/ID_find/verify_code', methods=['POST'])
+def verify_code_and_get_id():
+    try:
+        phone_number = session.get('phone_number')
+        if phone_number is None:
+            return jsonify({"error": "휴대전화 번호가 없습니다. 인증 코드 요청부터 다시 시작해주세요."}), 400
+        user_code = request.json.get('code')
+        if not phone_number or not user_code:
+            return jsonify({"error": "전화번호와 인증 코드는 필수입니다"}), 400
+
+        conn = mysql.connector.connect(**db_config)
+        cursor = conn.cursor(buffered=True)
+        cursor.execute("SELECT VerificationCode, Expiry FROM phone_verification WHERE PhoneNumber=%s", (phone_number,))
+        actual_code, expiry = cursor.fetchone()
+
+        if datetime.datetime.now() > expiry:
+            cursor.close()
+            conn.close()
+            return jsonify({"error": "인증 코드가 만료되었습니다"}), 400
+
+        if user_code != actual_code:
+            cursor.close()
+            conn.close()
+            return jsonify({"error": "인증 코드가 일치하지 않습니다"}), 400
+
+        cursor.execute("SELECT Email_ID FROM member_user WHERE PhoneNumber=%s", (phone_number,))
+        email = cursor.fetchone()[0]
+        cursor.close()
+        conn.close()
+        return jsonify({"email": email, "message": "인증에 성공하였습니다"}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+
+# ----------------------회원정보입력---------------------------#
+
+@app.route('/signup', methods=['POST'])
+def signup():
+    data = request.get_json()
+
+    # 필수 필드 확인
+    required_fields = ['Name', 'Password', 'ConfirmPassword','PhoneNumber', 
+                       'BusinessRegistrationNumber', 'CompanyAddress', 'EstablishedDate', 'CEO', 'CompanySize', 
+                       'CompanyType', 'EmployeeCount', 'InterestKeywords']
+    
+    if not all(field in data for field in required_fields):
+        return jsonify({'error': '모든 필드를 입력해주세요'}), 400
+
+    name = data['Name']
+    password = data['Password']
+    confirm_password = data['ConfirmPassword']
+
+    if password != confirm_password:
+        return jsonify({'error': '비밀번호가 일치하지 않습니다'}), 400
+    
+    phone = data['PhoneNumber']
+    company_info = {
+        'BusinessRegistrationNumber': data['BusinessRegistrationNumber'],
+        'CompanyAddress': data['CompanyAddress'],
+        'EstablishedDate': data['EstablishedDate'],
+        'CEO': data['CEO'],
+        'CompanySize': data['CompanySize'],
+        'CompanyType': data['CompanyType'],
+        'EmployeeCount': data['EmployeeCount'],
+        'InterestKeywords': data['InterestKeywords']
+    }
+
+    
+    # 세션에서 이메일과 약관 동의 정보 가져오기
+    email = session.get('email')
+    agree_service = session.get('agree_service')
+    agree_privacy = session.get('agree_privacy')
+    agree_marketing = session.get('agree_marketing')
+    verified = session.get('verified')
+
+    if not all([email, agree_service, agree_privacy, verified]):
+        return jsonify({'error': '이메일 인증과 약관 동의를 먼저 완료해주세요'}), 400
+
+    # try:
+        # MariaDB 연결
+    conn = mysql.connector.connect(**db_config)
+    cursor = conn.cursor()
+
+    # 회원가입 정보 저장
+    insert_user_query = 'INSERT INTO member_user (Name, Email_ID, PhoneNumber) VALUES (%s, %s, %s)'
+    cursor.execute(insert_user_query, (name, email, phone))
+    member_no = cursor.lastrowid  # 새로 생성된 회원 번호 가져오기
+
+    insert_auth_query = 'INSERT INTO member_authenticationinfo (MemberNo, Password) VALUES (%s, %s)'
+    cursor.execute(insert_auth_query, (member_no, password))
+
+    # 기업 정보 저장
+    insert_company_query = '''
+    INSERT INTO member_company (BusinessRegistrationNumber, MemberNo, CompanyAddress, EstablishedDate, 
+                                CEO, CompanySize, CompanyType, EmployeeCount, 
+                                InterestKeywords)
+    VALUES (%s, %s, %s, %s, 
+            %s, %s, %s, %s, 
+            %s)
+    '''
+    cursor.execute(insert_company_query, 
+                    (company_info['BusinessRegistrationNumber'], member_no, company_info['CompanyAddress'], 
+                    company_info['EstablishedDate'], company_info['CEO'], company_info['CompanySize'], 
+                    company_info['CompanyType'], company_info['EmployeeCount'], company_info['InterestKeywords']
+                    ))
+
+    # 약관 동의 정보 저장
+    insert_agreement_query = 'INSERT INTO agreement (MemberNo, AgreeService, AgreePrivacy, AgreeMarketing) VALUES (%s, %s, %s, %s)'
+    cursor.execute(insert_agreement_query, (member_no, agree_service, agree_privacy, agree_marketing))
+
+    conn.commit()
+
+    cursor.close()
+    conn.close()
+
+    # 세션에서 이메일과 약관 동의 정보 삭제
+    for key in ['email', 'agree_service', 'agree_privacy', 'agree_marketing', 'verification_code']:
+        session.pop(key, None)
+
+    return jsonify({'message': '회원가입이 완료되었습니다'}), 201
+    
+    # except Exception as e:
+    #     print(f"Exception occurred: {e}")
+    #     return jsonify({'error': '서버 오류가 발생했습니다'}), 500
+
+# 이메일 중복확인
+@app.route('/accounts/check_email', methods=['GET'])
+def check_email():
+    data = request.get_json()
+    email = data.get('email')
+
+    if not email:
+        return jsonify({'error': 'Email is required'}), 400
+
+    sql = '''
+            SELECT COUNT(*)
+            FROM MEMBER_USER
+            WHERE Email_ID = %s
+          '''
+    
+    conn = mysql.connector.connect(**db_config)
+    cursor = conn.cursor()
+
+    cursor.execute(sql, (email,))
+    result = cursor.fetchone()
+
+    cursor.close()
+    conn.close()
+
+    if result[0] > 0:
+        return jsonify({"result":"error", "description":"이메일 중복"})
+    else:
+        return jsonify({"result":"success", "description":"이메일 사용가능"})
+
+@app.route('/post/lists', methods=['GET'])
+def get_post_list():
+    
+    sql = '''
+            SELECT PostID, organization, notice, apply_end, tag, budget, views
+            FROM POSTS
+          '''
+
+    conn = mysql.connector.connect(**db_config)
+    cursor = conn.cursor()
+    cursor.execute(sql)
+    posts = cursor.fetchall()
+
+    posts_list = []
+    for post in posts:
+        # print(post[3])
+        if (post[3]=='-'):
+            days_left = '확정안됨'
+        else:
+            deadline = datetime.strptime(post[3], '%Y-%m-%d').date()
+            today = datetime.now().date()
+            days_left = (today - deadline).days
+        
+        post_dict = {
+            'PostID': post[0],
+            'organization': post[1],
+            'notice': post[2], 
+            'apply_end': days_left,
+            'tag': post[4],
+            'budget': post[5],
+            'views': post[6],
+        }
+        posts_list.append(post_dict)
+    total_count = len(posts_list)
+    return jsonify({"meta": {"total_count": total_count}, "documents": posts_list})
+
+@app.route('/post/lists/<int:PostID>/download/<string:pfi_originname>')
+def download_attachment(PostID, pfi_originname):
+    conn = mysql.connector.connect(**db_config)
+    cursor = conn.cursor()
+
+    sql = '''
+            SELECT pfi_originname, pfi_filename
+            FROM POST_FILE
+            WHERE PostID = %s and pfi_originname = %s
+          '''
+        
+    cursor.execute(sql, (PostID, pfi_originname,))
+    attachment = cursor.fetchone()
+    pfi_originname = attachment[0]
+    pfi_filename = attachment[1]
+    attachment_folder = 'attachment_folder'
+    script_dir = os.path.dirname(__file__)
+    filepath = os.path.join(script_dir, attachment_folder, pfi_filename)
+    #print('filepath', filepath)
+    return send_file(filepath, as_attachment=True, download_name=pfi_originname)
+
+@app.route('/post/lists/<int:PostID>', methods=['GET'])
+def get_post(PostID):
+    conn = mysql.connector.connect(**db_config)
+    cursor = conn.cursor()
+
+    # 조회수 증가
+    update_sql = '''
+            UPDATE POSTS SET views = views + 1
+            WHERE PostID = %s
+          '''
+    cursor.execute(update_sql, (PostID,))
+    conn.commit()
+
+    sql = '''
+            SELECT PostID, part, `object`, department, organization, post_date, notice, apply_start, apply_end, budget, overview
+            FROM POSTS
+            WHERE PostID = %s
+          '''
+
+    cursor.execute(sql, (PostID,))
+    post = cursor.fetchall()
+    post = post[0]
+    
+    #print('post', post)
+    # 첨부파일
+    sql = '''
+            SELECT pfi_originname, pfi_filename
+            FROM POST_FILE
+            WHERE PostID = %s
+          '''
+
+    cursor.execute(sql, (PostID,))
+    attachments = cursor.fetchall()
+    attachment_list = []
+    for attachment in attachments:
+        attachment_list.append(
+            {
+                'pfi_originname':attachment[0],
+                'pfi_filename':attachment[1]
+            }
+        )
+    
+    # print('attachment_list', attachment_list)
+    
+    if (post[8]=='-'):
+            days_left = '확정안됨'
+    else:
+        deadline = datetime.strptime(post[8], '%Y-%m-%d').date()
+        today = datetime.now().date()
+        days_left = (today - deadline).days
+    
+    post_dict = {
+            'PostID': post[0],
+            'part': post[1],
+            'object' : post[2],
+            'department': post[3],
+            'organization': post[4],
+            'post_date': post[5],
+            'notice': post[6], 
+            'days_left': days_left,
+            'apply_start': post[7],
+            'apply_end': post[8],
+            'budget': post[9],
+            'overview': post[10],
+            'attachments': attachment_list
+    }
+    return jsonify({"document" : post_dict})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+@app.route('/send_password_reset_code', methods=['POST'])
+def send_password_reset_code():
+    data = request.get_json()
+    phone_number = data.get('PhoneNumber')
+    email = data.get('Email_ID')
+
+    # connect to the database
+    conn = mysql.connector.connect(**db_config)
+    cursor = conn.cursor()
+
+    # check if the phone number and email exist in the member_user table
+    cursor.execute('SELECT * FROM member_user WHERE PhoneNumber=%s AND Email_ID=%s', (phone_number, email))
+    if cursor.fetchone() is None:
+        return jsonify({'error': '등록되지 않은 전화번호 또는 이메일입니다'}), 400
+
+    # generate a random verification code
+    verification_code = '123456'
+
+    # calculate the expiry time for the verification code
+    expiry = datetime.datetime.now() + datetime.timedelta(minutes=10)
+
+    # insert the verification code into the phone_verification table
+    cursor.execute(
+        'INSERT INTO phone_verification (PhoneNumber, VerificationCode, Expiry) VALUES (%s, %s, %s)',
+        (phone_number, verification_code, expiry)
+    )
+
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+    return jsonify({'message': '인증 코드가 발송되었습니다'}), 200
